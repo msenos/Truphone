7992fed149612ed68552e70de34482addac98e90
Miguel Senos <msenos@outlook.com>
2023-01-17 15:12:39 +0000

    added entities to Domain

---
+++ Truphone.Domain/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace Truphone.Domain
+{
+    public class Brand : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
---
+++ Truphone.Domain/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Truphone.Domain
+{
+    public class Device : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public Brand Brand { get; set; }
+
+        public Device()
+        {
+            Brand = new Brand();
+        }
+    }
+}
