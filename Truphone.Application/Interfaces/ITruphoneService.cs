364101b3704d868d9d087ac82668fb35439d6f3a
Miguel Senos <msenos@outlook.com>
2023-01-17 15:22:53 +0000

    Added Interfaces and service

---
+++ Truphone.Application/Interfaces/ITruphoneService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truphone.Application.Interfaces
+{
+    public interface ITruphoneService
+    {
+        List<Domain.Device> GetAllDevices();
+    }
+}
